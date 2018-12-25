05c896ad18bafa5ea8755769220a339f74cc3776
Chance <playgamestimes@gmail.com>
2018-12-25 10:59:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
+
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -10,6 +8,7 @@ import java.net.MalformedURLException;
 public class Main {
     private static String INPUT_OF_FILE = "src\\inFile.txt";
     private static String PATH_TO_MUSIC = "";
+
     public static void main(String[] args) throws InterruptedException {
         String Url;
         String file;
@@ -22,15 +21,14 @@ public class Main {
                     count++;
                 }
             }
-
-            Download downloadMusicParallel = new Download(array[0], array[1]);
-            Download downloadPictureParallel = new Download(array[2], array[3]);
             PATH_TO_MUSIC = array[3];
+            Download downloadMusicParallel = new Download(array[0], array[1], "jpg");
+            Download downloadPictureParallel = new Download(array[2], PATH_TO_MUSIC, "mp3");
+
 
             downloadPictureParallel.start();
             downloadMusicParallel.start();
 
-            play(array);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -38,16 +36,8 @@ public class Main {
         }
 
     }
-    public static void play(String[] array) {
-        try (FileInputStream inputStream = new FileInputStream(  PATH_TO_MUSIC)) {
-            try {
-                Player player = new Player(inputStream);
-                player.play();
-            } catch (JavaLayerException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    public static void play() {
+
     }
 }
